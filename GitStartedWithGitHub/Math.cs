102f310bb58333d1c09f79ebfbcd59f28d8547e9
Robert <rgreen2005@msn.com>
2022-04-05 16:41:07 -0600

    Added Add method

--- GitStartedWithGitHub/Math.cs
+++ GitStartedWithGitHub/Math.cs
@@ -8,5 +8,12 @@ namespace GitStartedWithGitHub
 {
     internal class Math
     {
+        internal int Add()
+        {
+            int x1 = 1;
+            int x2 = 2;
+            int sum = x1 + x2;
+            return sum;
+        }
     }
 }
